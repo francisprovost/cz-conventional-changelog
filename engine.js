34b0772edb2699f968c17608b8bc3c2681dd2dad
Francis Provost <francis.provost@gmail.com>
2017-11-15 16:04:00 -0500

    feat: Update config for initial needs

--- engine.js
+++ engine.js
@@ -56,10 +56,6 @@ module.exports = function (options) {
           choices: choices
         }, {
           type: 'input',
-          name: 'scope',
-          message: 'What is the scope of this change (e.g. component or file name)? (press enter to skip)\n'
-        }, {
-          type: 'input',
           name: 'subject',
           message: 'Write a short, imperative tense description of the change:\n'
         }, {
@@ -68,25 +64,13 @@ module.exports = function (options) {
           message: 'Provide a longer description of the change: (press enter to skip)\n'
         }, {
           type: 'confirm',
-          name: 'isBreaking',
-          message: 'Are there any breaking changes?',
-          default: false
-        }, {
-          type: 'input',
-          name: 'breaking',
-          message: 'Describe the breaking changes:\n',
-          when: function(answers) {
-            return answers.isBreaking;
-          }
-        }, {
-          type: 'confirm',
           name: 'isIssueAffected',
           message: 'Does this change affect any open issues?',
           default: false
         }, {
           type: 'input',
           name: 'issues',
-          message: 'Add issue references (e.g. "fix #123", "re #123".):\n',
+          message: 'Add issue references (e.g. Jira ticket ID.):\n',
           when: function(answers) {
             return answers.isIssueAffected;
           }
@@ -102,24 +86,15 @@ module.exports = function (options) {
           width: maxLineWidth
         };
 
-        // parentheses are only needed when a scope is present
-        var scope = answers.scope.trim();
-        scope = scope ? '(' + answers.scope.trim() + ')' : '';
-
-        // Hard limit this line
-        var head = (answers.type + scope + ': ' + answers.subject.trim()).slice(0, maxLineWidth);
+        var issueBlock = answers.issues ? `/${answers.issues}` : '';
+        var header = `${answers.type}${issueBlock}: ${answers.subject.trim().slice(0, maxLineWidth)}`;
 
         // Wrap these lines at 100 characters
         var body = wrap(answers.body, wrapOptions);
 
-        // Apply breaking change prefix, removing it if already present
-        var breaking = answers.breaking ? answers.breaking.trim() : '';
-        breaking = breaking ? 'BREAKING CHANGE: ' + breaking.replace(/^BREAKING CHANGE: /, '') : '';
-        breaking = wrap(breaking, wrapOptions);
+        var issueLink = answers.issues ? `https://jira.sm360.ca/browse/${answers.issues}` : '';
 
-        var issues = answers.issues ? wrap(answers.issues, wrapOptions) : '';
-
-        var footer = filter([ breaking, issues ]).join('\n\n');
+        var footer = `issue: ${issueLink}`;
 
         commit(head + '\n\n' + body + '\n\n' + footer);
       });
